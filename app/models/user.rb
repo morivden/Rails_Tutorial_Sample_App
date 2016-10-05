21c0a2a3ff93c2e5827472f6f9d5c16ba7cd0576
morivden <fuziyamavolcano@yahoo.co.jp>
2016-10-05 09:15:12 +0900

    tutorial7 start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
     # email属性を小文字に変換してメールアドレスの一意性を保証
-    before_save {self.email = email.downcase}
+    before_save {email.downcase!}
     # name属性の存在性を検証
     validates :name, presence: true, length: {maximum: 50}
     # validates(:name, presence: true)
     # メールフォーマットを正規表現で検証
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     # email属性の存在性を検証
     # メールアドレスの一意性を検証
     validates :email, presence: true, length: {maximum: 255},
