b31a8a724876c428e84a740dade432c17f84004a
morivden <fuziyamavolcano@yahoo.co.jp>
2016-10-26 17:09:35 +0900

    tutorial 8.4 start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+    attr_accessor :remember_token
     # email属性を小文字に変換してメールアドレスの一意性を保証
     before_save {email.downcase!}
     # name属性の存在性を検証
@@ -13,4 +14,22 @@ class User < ActiveRecord::Base
                 uniqueness: {case_sensitive: false}
     has_secure_password
     validates :password, presence: true, length: {minimum: 6}
+
+    # 与えられた文字列のハッシュ値を返す
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ?
+        BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    # ランダムなトークンを返す
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # 永続的セッションで使用するユーザーをデータベースに記憶
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
 end
